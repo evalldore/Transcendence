ab6ffae2aac7104ad6ca22a824d57b457b994ddc
evalldore <evalldore@gmail.com>
2024-11-05 17:33:07 -0500

    engine structure

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 import * as THREE from 'three';
 
+var clock;
+var camera;
+var scene;
+var renderer;
+var paddleMaterial;
+var mesh;
+var running = true;
+const speed = 5;
+const canvasScale = 12;
 
 const inputs = {
 	up: false,
@@ -7,39 +16,57 @@ const inputs = {
 	left: false,
 	right: false
 }
-const clock = new THREE.Clock();
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-function render() {
-
-	let delta = clock.getDelta();
-
-	if (inputs.left)
-		cube.rotation.x -= 1 * delta;
-	if (inputs.right)
-		cube.rotation.x += 1 * delta;
-	if (inputs.up)
-		cube.rotation.y -= 1 * delta;
-	if (inputs.down)
-		cube.rotation.y += 1 * delta;
-
-	renderer.render( scene, camera );
-
+function createSquare(width, height, material) {
+	const vec = new THREE.Vector2(-1.0, 1.0).applyMatrix3;
+	const geometry = new THREE.BufferGeometry(); 
+	const vertices = new Float32Array( [ -width / 2, -height / 2, width / 2, -height / 2, width / 2, height / 2, -width / 2, height / 2 ] ); 
+	geometry.setIndex( [ 0, 1, 2, 2, 3, 0 ] ); 
+	geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 2 ) ); 
+	var square = new THREE.Mesh( geometry, material );
+	return square;
 }
 
-renderer.setAnimationLoop( render );
+function init() {
+	let canvasRatio = window.innerWidth / window.innerHeight;
+	let canvasWidth = canvasScale * canvasRatio;
+	let canvasHeight = canvasScale;
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+
+	clock = new THREE.Clock();
+
+	camera = new THREE.OrthographicCamera( canvasWidth / -2, canvasWidth / 2, canvasHeight / 2, canvasHeight / -2, 0.1, 1000 );
+	camera.position.x = canvasWidth / 2;
+	camera.position.y = -canvasHeight / 2;
+	camera.position.z = 20;
+
+	paddleMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+	scene = new THREE.Scene();
+	mesh = createSquare(0.5, 2, paddleMaterial);
+	mesh.position.x = 0.25;
+	mesh.position.y = canvasScale / -2;
+	scene.add(mesh);
+}
+
+function update(deltaTime) {
+
+	let speedDelta = speed * deltaTime;
+	if (inputs.up)
+		mesh.position.y += speedDelta;
+	if (inputs.down)
+		mesh.position.y -= speedDelta;
+
+	console.log(mesh.position);
+}
+
+function render() {
+	update(clock.getDelta());
+	renderer.render(scene, camera);
+}
 
 function keyDown(event) {
 	switch(event.key) {
@@ -62,7 +89,6 @@ function keyDown(event) {
 
 window.addEventListener("keydown", keyDown);
 
-
 function keyUp(event) {
 	switch(event.key) {
 		case "ArrowDown":
@@ -83,3 +109,10 @@ function keyUp(event) {
 }
 
 window.addEventListener("keyup", keyUp);
+
+try {
+	init();
+	renderer.setAnimationLoop( render );
+} catch(e) {
+	console.log(e);
+}
